894398176669cc0cad81f7961118aec1fbe6552b
Hazeem42 <oshoalakorede42@gmail.com>
2022-07-04 05:24:43 -0700

    Hash tables

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,18 +10,18 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *ht;
-	unsigned long int i;
+
+	if (size == 0)
+		return (NULL);
 
 	ht = malloc(sizeof(hash_table_t));
-	if (ht == NULL)
+	if (!ht)
 		return (NULL);
 
 	ht->size = size;
-	ht->array = malloc(sizeof(hash_node_t *) * size);
+	ht->array = calloc((size_t)ht->size, sizeof(hash_node_t *));
 	if (ht->array == NULL)
 		return (NULL);
-	for (i = 0; i < size; i++)
-		ht->array[i] = NULL;
 
 	return (ht);
 }
